d335efbcbb823c25aa69f7d02954eca1e8739f25
8bittouche <8bittouche@gmail.com>
2019-09-23 17:38:45 +0900

    BOJ 11651

---
+++ BOJ/BOJ 11651 좌표 정렬하기2/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool comp(pair<int, int> a, pair<int, int> b) {
+	if (a.second == b.second) {
+		return a.first < b.first;
+	}
+	return a.second < b.second;
+}
+
+int main(void) {
+	vector<pair<int, int>> coordinates;
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; ++i) {
+		int x, y;
+		scanf("%d %d", &x, &y);
+		coordinates.push_back(make_pair(x, y));
+	}
+	sort(coordinates.begin(), coordinates.end(), comp);
+	for (int i = 0; i < n; ++i) {
+		printf("%d %d\n", coordinates[i].first, coordinates[i].second);
+	}
+	return 0;
+}
