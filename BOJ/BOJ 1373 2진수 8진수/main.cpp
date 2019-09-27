cfa54fd93e5c2795c0b3069a25f865bb04d6fc28
8bittouche <8bittouche@gmail.com>
2019-09-27 18:33:10 +0900

    BOJ 1373

---
+++ BOJ/BOJ 1373 2진수 8진수/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	
+	string ans = "";
+	int len = s.length();
+	if (len % 3 == 1) {
+		cout << s[0]-'0';
+	}
+	else if (len % 3 == 2) {
+		cout << (s[0]-'0') * 2 + (s[1]-'0');
+	}
+	for (int i = len % 3; i < len; i += 3) {
+		cout << (s[i]-'0') * 4 + (s[i + 1]-'0') * 2 + (s[i + 2]-'0');
+	}
+
+	
+	return 0;
+}
