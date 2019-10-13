7fb735d72671d06f1db7899c547963d5f9cdeac0
8bittouche <8bittouche@gmail.com>
2019-10-13 13:00:07 +0900

    BOJ 1929

---
+++ BOJ/BOJ 1929 소수구하기/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static boolean isPrime(int n) {
+		if(n<2) {
+			return false;
+		}
+		else if(n==2 || n==3) {
+			return true;
+		}
+		
+		for(int i=2; i<=Math.sqrt(n); ++i) {
+			if(n%i==0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int cnt=0;
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		for(int i=m; i<=n; ++i) {
+			if(isPrime(i)) {
+				System.out.println(i);
+			}
+		}
+	}
+}
