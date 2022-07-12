ba10308402259c3a71b6d41a8f794d81f048051e
8bittouche <8bittouche@gmail.com>
2022-07-12 13:13:07 +0900

    refactoring code

---
+++ leetcode/1672. Richest Customer Wealth/Solution2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+
+        for(int[] customer : accounts){
+            int sum = 0;
+
+            for(int bank : customer){
+                sum += bank;
+            }
+
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+
+    // Time Complexity = O(n x m)
+    // Space Complexity = O(1)
+}
