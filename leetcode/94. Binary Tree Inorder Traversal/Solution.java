8ff8c60099941cb016cce618eee8a811651c24f8
8bittouche <8bittouche@gmail.com>
2022-07-26 16:29:39 +0900

    solve Binary Tree Inorder Traversal

---
+++ leetcode/94. Binary Tree Inorder Traversal/Solution.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void func(TreeNode root, List<Integer> ans){
+        if(root == null){
+            return;
+        }
+
+        if(root.left != null){
+            func(root.left, ans);
+        }
+
+        ans.add(root.val);
+
+        if(root.right != null){
+            func(root.right, ans);
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+
+        func(root, ans);
+
+        return ans;
+    }
+}
