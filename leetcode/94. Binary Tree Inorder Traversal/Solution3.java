12adc3c7af63b36313772ca6988ac0ff047f70dc
8bittouche <8bittouche@gmail.com>
2022-07-26 16:47:15 +0900

    solution using stack

---
+++ leetcode/94. Binary Tree Inorder Traversal/Solution3.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+
+        while(cur != null || !stack.empty()){
+            while(cur != null){
+                stack.add(cur);
+                cur = cur.left;
+            }
+
+            cur = stack.pop();
+            ans.add(cur.val);
+
+            cur = cur.right;
+        }
+
+        return ans;
+    }
+}
